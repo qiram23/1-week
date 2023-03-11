ca257cba45182a1897bffb9ec7a98f38b90d87ba
qiram23 <44774560+qiram23@users.noreply.github.com>
2023-03-11 03:46:34 +0300

    Add files via upload

---
+++ iterTuple.cpp
@@ -0,0 +1,28 @@
+﻿
+#include<iostream>
+#include<tuple>
+
+using namespace std;
+
+template <typename T>
+void print_element(const T& x) {
+  cout << x << " ";
+};
+
+template <typename TupleT, size_t... Is>
+void print_tuple_manual(const TupleT& tp, index_sequence<Is...>) {
+  (print_elem(get<Is>(tp)), ...);
+}
+
+template <typename TupleT, size_t TupSize = tuple_size_v<TupleT>>
+void print_tuple(const TupleT& tp) {
+  print_tuple_manual(tp, make_index_sequence<TupSize>{});
+}
+
+
+int main() 
+{
+  tuple a{1, a, tupel};
+  print_tuple(a);
+  cout << '\n';
+}
